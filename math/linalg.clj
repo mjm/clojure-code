cac420755bc9ef100d86656d19bbaa907820fa2a
Matt Moriarity <matt.moriarity@gmail.com>
2009-02-03 13:38:35 -0500

    Added some row reduction code

--- math/linalg.clj
+++ math/linalg.clj
@@ -445,6 +445,104 @@
     (mult (/ 1 invert)
           (adjoint m))))
 
+;;; ELEMENTARY ROW OPERATIONS
+
+(defn swap-rows
+  "Swaps two rows of a matrix"
+  [m r1 r2]
+  (gen-matrix (:rows m)
+              (:cols m)
+              (fn [i j]
+                (mget m
+                      (condp = i r1 r2 r2 r1 i)
+                      j))))
+
+(defn multiply-row
+  "Multiplies a row of a matrix by a scalar"
+  [m r n]
+  (assert (not= 0 n))
+  (gen-matrix (:rows m)
+              (:cols m)
+              (fn [i j]
+                (if (= i r)
+                  (* n (mget m i j))
+                  (mget m i j)))))
+
+(defn add-mult
+  "Adds a multiple of one row to another row"
+  [m n r1 r2]
+  (gen-matrix (:rows m)
+              (:cols m)
+              (fn [i j]
+                (if (= i r2)
+                  (+ (mget m i j)
+                     (* n (mget m r1 j)))
+                  (mget m i j)))))
+
+;;; ROW REDUCTION
+;;; Warning: These have big flaws right now.
+;;; They don't handle zeros in the wrong places.
+;;; They also will gladly divide by zero if given the chance.
+
+(defn lower-reduce-column
+  "Applies row operations to reduce the lower half of the matrix to
+  zeroes. Assumes that the previous columns have already been reduced."
+  [m c]
+  (let [mr (multiply-row m c (/ 1 (mget m c c)))]
+    (loop [acc mr
+           r (inc c)]
+      (if (> r (:rows m))
+        acc
+        (recur
+         (add-mult acc
+                   (- (mget acc r c))
+                   c
+                   r)
+         (inc r))))))
+
+(defn lower-reduce
+  "Row-reduces a matrix so it is upper-triangular (reduce the lower half
+  to zeroes)."
+  [m]
+  (reduce lower-reduce-column
+          m
+          (range 1 (inc (min (:rows m) (:cols m))))))
+
+(defn upper-reduce-column
+  "Applies row operations to reduce the upper half of the matrix to
+  zeroes. Assumes that subsequent columns have already been reduced."
+  [m c]
+  (let [mr (multiply-row m c (/ 1 (mget m c c)))]
+    (loop [acc mr
+           r (dec c)]
+      (if (< r 1)
+        acc
+        (recur
+         (add-mult acc
+                   (- (mget acc r c))
+                   c r)
+         (dec r))))))
+
+(defn upper-reduce
+  "Row-reduces a matrix so it is lower-triangular (reduce the upper half
+  to zeroes)."
+  [m]
+  (reduce upper-reduce-column
+          m
+          (range (min (:rows m) (:cols m)) 0 -1)))
+
+(defn rref
+  "Gives the matrix in reduced row echelon form."
+  [m] (upper-reduce (lower-reduce m)))
+
+(defn augment
+  "Puts two matrices side-by-side in a new matrix."
+  [m1 m2]
+  (assert (= (:rows m1) (:rows m2)))
+  )
+
+;;; PRINTING MATRICES AND VECTORS
+
 (defmulti
   #^{:doc "Prints the individual items in a matrix into 7-character wide
   columns separated by single spaces."}
