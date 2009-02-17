cff550be7e271822d6435177e7864abfde1e7994
Matt Moriarity <matt.moriarity@gmail.com>
2009-02-17 13:38:15 -0500

    Added more rules and actually use the rule translator

--- math/symbolic.clj
+++ math/symbolic.clj
@@ -1,14 +1,35 @@
 (ns math.symbolic
   (:use math
+        patmatch
         clojure.contrib.test-is))
 
 (def simp-rules
-     '(((+ x 0) x)
-       ((+ 0 x) x)
-       ((+ x x) (* 2 x))
-       ((- x 0) x)
-       ((- 0 x) (- x))
-       ((- x x) 0)))
+     '{(+ x 0) x
+       (+ 0 x) x
+       (+ x x) (* 2 x)
+       (- x 0) x
+       (- 0 x) (- x)
+       (- x x) 0
+       (- (- x)) x
+       (* x 1) x
+       (* 1 x) x
+       (* x 0) 0
+       (* 0 x) 0
+       (* x x) (** x 2)
+       (/ 0 x) 0
+       (/ x 1) x
+       (/ x x) 1
+       (** x 0) 1
+       (** 0 x) 0
+       (** 1 x) 1
+       (** x 1) x
+       (** x -1) (/ 1 x)
+       (* x (/ y x)) y
+       (* (/ y x) x) y
+       (/ (* y x) x) y
+       (/ (* x y) x) y
+       (+ x (- x)) 0
+       (+ x (- y x)) y})
 
 (defn- can-eval-op?
   [op]
@@ -30,11 +51,15 @@
 
 (defn simplify-exp
   [exp]
-  (if (can-eval? exp)
-    (eval exp)
-    exp))
+  (or (translate-by-rules exp simp-rules #(simplify (substitute % %2)))
+      (if (can-eval? exp) (eval exp)
+          exp)))
 
 (defn simplify [exp]
   (if (coll? exp)
     (simplify-exp (map simplify exp))
     exp))
+
+(defmacro math [& body]
+  `(with-vars [x y z m n o p q r s t u v w]
+     ~@body))
