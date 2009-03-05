1ce700b4d640a0561f9fa53989f3e68e07ee5c33
Matt Moriarity <matt.moriarity@gmail.com>
2009-03-04 20:16:05 -0500

    Added an implementation of Prim's algorithm for minimal spanning trees

--- structures/graph.clj
+++ structures/graph.clj
@@ -1,4 +1,5 @@
 (ns structures.graph
+  (:use clojure.set)
   (:import clojure.lang.PersistentQueue))
 
 ;; {:a [:b :c], :b [:a :c], :c [:a :d], :d [:c]}
@@ -24,3 +25,47 @@
 
 (defn shortest-path [graph start end]
   (bfs graph (conj PersistentQueue/EMPTY (list start)) #(= %1 end) #{}))
+
+;; Debug Graph:
+(comment
+  (def g (-> empty-el-graph
+             (add-edge :a :b 5)
+             (add-edge :a :d 2)
+             (add-edge :b :c 3)
+             (add-edge :b :d 1)
+             (add-edge :c :d 1))))
+
+(defstruct el-graph :edges :vertices)
+
+(def empty-el-graph (struct el-graph #{} #{}))
+
+(defn edge [from to weight]
+  [from to weight])
+
+(defn add-edge
+  ([graph] graph)
+  ([graph edge]
+     (apply add-edge graph edge))
+  ([graph from to weight]
+     (assoc graph
+       :edges (union (graph :edges) #{(edge from to weight)
+                                      (edge to from weight)})
+       :vertices (union (graph :vertices) #{from to}))))
+
+(defn edges-to-check [graph used]
+  (filter (fn [[u v w]]
+            (and (contains? used u)
+                 (not (contains? used v))))
+          (graph :edges)))
+
+(defn next-edge [graph used]
+  (if-let [edges (seq (edges-to-check graph used))]
+    (apply min-key (fn [[u v w]] w) edges)))
+
+(defn mst [graph start]
+  (loop [tree empty-el-graph
+         used #{start}]
+    (if (= used (graph :vertices))
+      tree
+      (let [new-tree (add-edge tree (next-edge graph used))]
+        (recur new-tree (tree :vertices))))))
